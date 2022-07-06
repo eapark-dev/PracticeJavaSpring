f4479005dff13d6aed2a2298fb1d7a4859559705
eapark-dev <eapark5908@gmail.com>
2022-07-06 21:22:59 +0900

    JPA 스프링 부트와 JPA 활용 1 - 회원 기능 테스트 완료

---
+++ jpashop/src/test/java/japbook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,54 @@
+package japbook.jpashop.service;
+
+import japbook.jpashop.domain.Member;
+import japbook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
+
+    @Test
+    @Rollback(false)
+    public void 회원가입() throws Exception {
+
+        Member member = new Member();
+        member.setName("park");
+
+        Long savedId = memberService.join(member);
+
+        em.flush();
+
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
+
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        memberService.join(member1);
+        memberService.join(member2); //중복일 경우 예외 발생
+
+        fail("예외 발생");
+    }
+}

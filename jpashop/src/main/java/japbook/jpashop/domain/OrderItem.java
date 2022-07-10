62be7a6b4cd2104594a21a60fa3553c926acc8bd
eapark-dev <eapark5908@gmail.com>
2022-07-10 16:40:27 +0900

    JPA 스프링 부트와 JPA 활용 1 - 주문 도메인 개발 세션 완료

--- jpashop/src/main/java/japbook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/japbook/jpashop/domain/OrderItem.java
@@ -1,6 +1,8 @@
 package japbook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import japbook.jpashop.domain.item.Item;
@@ -9,6 +11,7 @@ import javax.persistence.*;
 @Entity
 @Table(name = "order_item")
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -22,4 +25,27 @@ public class OrderItem {
     @JoinColumn(name = "order_id")
     private Order order; //주문
     private int orderPrice; //주문 가격 private int count; //주문 수량
+    private int count; //주문 수량
+
+    //생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //비즈니스 로직
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //조회 로직
+    //주문 상품 전체 가격조회
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
